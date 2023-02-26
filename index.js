624dee37e19502286a25d75b0117467f926cf6c9
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-02-26 12:54:04 +0330

    debug middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Logger = require("./middleware/Logger");
+const logger = require("./middleware/logger");
 // require("dotenv").config();
 const dotenv = require("dotenv");
 const connectDB = require("./db/connect");
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,5 @@
-const Logger = (req, res, next) => {
+const logger = (req, res, next) => {
   console.log(`the api has a new request.METHOD: ${req.method}, URL: ${req.url}`);
   next();
 };
-module.exports = Logger;
+module.exports = logger;
